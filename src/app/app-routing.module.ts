89020afb681f33082b97471305091fb7247ba7d7
Aga <mishyag@gmail.com>
2018-12-09 22:48:36 +0100

    poczatki frontendu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ErrorComponent } from './error/error.component';
+import { BooksComponent } from './books/books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'welcome/:name', component: WelcomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'books', component: BooksComponent},
+  {path: '**', component: ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+export class Book{
+  constructor(
+    public id : number,
+    public title: string,
+    public autor: string,
+    public description: string,
+    public price: number
+
+  ){
+
+  }
+
+}
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+
+  books =[
+    new Book( 1, 'zenek w lesie','zenek', 'cos tam', 20.0),
+    new Book( 2, 'zenek na morzu','zenek', 'cos tam', 20.0),
+    new Book( 3, 'zenek na pustyni','zenek', 'cos tam', 20.0)
+   
+
+  ]
+  // book = {
+  //   id : 1,
+  //   title : 'ksiazka',
+  //   autor : 'zenek zielony',
+  //   description: 'brak uszkodzen',
+  //   price: 11.0
+  // }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
